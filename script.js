6928cd6d8f44798a26312ae714a85543e4a093a5
pstar8 <ifedaraniyi@gmail.com>
2024-05-01 07:47:23 -0700

    updated commit

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Javascript here
 
-function myFunction(password) {
-  let user = prompt('Enter Password');
-  if ('password.length < 6') {
+function myFunction() {
+  let password = prompt('Enter Password');
+  console.log(password);
+  if (password.length < 6) {
     console.log('Password is too short!!! Change it!');
     return password.length;
-  } else if ('password.length > 6 < 8') {
+  } else if (password.length > 6 && password.length < 8) {
     console.log('Password is poor, but manageabele. Change it abeg!');
     return password.length;
-  } else if ('password.length > 8 < 10') {
+  } else if (password.length > 8 && password.length < 10) {
     console.log('Fair password');
     return password.length;
-  } else if ('password.length > 10') {
+  } else if (password.length > 10) {
     console.log('Very strong password. Well done!!!');
     return password.length;
   }
